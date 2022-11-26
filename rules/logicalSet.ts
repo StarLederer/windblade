a43fb9611eafedaa8aa42bf1033a7cdfec1fb9d7
Herman Lederer <germans.lederers@gmail.com>
2022-11-26 15:42:12 +0100

    Fix logicalRuleSet

--- rules/logicalSet.ts
+++ rules/logicalSet.ts
@@ -1,20 +1,22 @@
 import type { Rule } from "@unocss/core";
 import { ITheme } from "../theme/types";
 
+const join = (arr: Array<any>) => (arr.filter(Boolean).join("-"));
+
 const logicalRuleSet = (
   prefix: string,
-  postfix: string,
+  postfix: string | undefined,
   propertyPrefix: string,
-  propertyPostfix: string,
+  propertyPostfix: string | undefined,
   rule: (prefix: string, property: string) => Rule<ITheme>
 ) => ([
-  rule(prefix, propertyPrefix),
-  rule(`${prefix}-b-${postfix}`, `${propertyPrefix}-block-${propertyPostfix}`),
-  rule(`${prefix}-bs-${postfix}`, `${propertyPrefix}-block-start-${propertyPostfix}`),
-  rule(`${prefix}-be-${postfix}`, `${propertyPrefix}-block-end-${propertyPostfix}`),
-  rule(`${prefix}-i-${postfix}`, `${propertyPrefix}-inline-${propertyPostfix}`),
-  rule(`${prefix}-is-${postfix}`, `${propertyPrefix}-inline-start-${propertyPostfix}`),
-  rule(`${prefix}-ie-${postfix}`, `${propertyPrefix}-inline-end-${propertyPostfix}`),
+  rule(join([prefix, postfix]), join([propertyPrefix, propertyPostfix])),
+  rule(join([prefix, "b", postfix]), join([propertyPrefix, "block", propertyPostfix])),
+  rule(join([prefix, "bs", postfix]), join([propertyPrefix, "block-start", propertyPostfix])),
+  rule(join([prefix, "be", postfix]), join([propertyPrefix, "block-end", propertyPostfix])),
+  rule(join([prefix, "i", postfix]), join([propertyPrefix, "inline", propertyPostfix])),
+  rule(join([prefix, "is", postfix]), join([propertyPrefix, "inline-start", propertyPostfix])),
+  rule(join([prefix, "ie", postfix]), join([propertyPrefix, "inline-end", propertyPostfix])),
 ]);
 
 export default logicalRuleSet;
