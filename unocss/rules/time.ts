7b770f89437d155d308efa17509968e4da519ab5
Herman Lederer <germans.lederers@gmail.com>
2023-01-24 22:30:25 +0100

    Implement time rules

---
+++ unocss/rules/time.ts
@@ -0,0 +1,37 @@
+import { DynamicRule } from "@unocss/core";
+import Theme from "../theme/Theme";
+
+const durationRule = (
+  prefix: string,
+  property: string,
+  value?: (size: string) => string
+): DynamicRule<Theme> => {
+  return [
+    new RegExp(`^(${prefix})-(.+)$`),
+    (match, { theme }) => {
+      const css: any = {};
+      let parameter = "" + theme.windblade.sizes.tokens[match[2]] * theme.windblade.time.baseUnitMs;
+      if (parameter === undefined) return undefined;
+      css[property] = value?.(parameter) ?? parameter;
+      return css;
+    }
+  ];
+};
+
+const timingFunctionRule = (
+  prefix: string,
+  property: string,
+): DynamicRule<Theme> => {
+  return [
+    new RegExp(`^(${prefix})-(.+)$`),
+    (match, { theme }) => {
+      const css: any = {};
+      let parameter = theme.windblade.time.functions[match[2]];
+      if (parameter === undefined) return undefined;
+      css[property] = parameter;
+      return css;
+    }
+  ];
+};
+
+export { durationRule, timingFunctionRule };
