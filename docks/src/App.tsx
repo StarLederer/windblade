d193816e0acf30f03809767c9c9c2523ece7bcde
Herman Lederer <germans.lederers@gmail.com>
2023-02-07 00:23:34 +0100

    Use theme store hues in App

--- docks/src/App.tsx
+++ docks/src/App.tsx
@@ -1,12 +1,12 @@
 import { Component } from "solid-js";
-import themeStore from "./stores/themeStore";
-import Root from "./tree/Root";
+import themeStore from "~/stores/themeStore";
+import Root from "~/tree/Root";
 
 const Main: Component = () => {
-  const themeStyles = () => themeStore.scheme() === "light" ? "scheme-light hue-220" : "scheme-dark hue-200";
+  const themeStyles = () => themeStore.scheme() === "light" ? "scheme-light" : "scheme-dark";
 
   return (
-    <div class={`${themeStyles()} size-i-full size-b-full relative bg-def3 text-fg-2 overflow-hidden`}>
+    <div class={`${themeStyles()} size-i-full size-b-full relative bg-def3 text-fg-2 overflow-hidden`} style={`--hue: ${themeStore.hue()};`}>
       <Root />
     </div>
   );
