ab259fd27fcfb7289170b4046d9fc0fcc16d048b
Herman Lederer <germans.lederers@gmail.com>
2023-02-08 12:17:34 +0100

    Add hue generator

---
+++ docks/src/lib/visuals.ts
@@ -0,0 +1,7 @@
+export const stringToHue = (str: string) => {
+  var hash = 0;
+  for (var i = 0; i < str.length; i++) {
+    hash = str.charCodeAt(i) + ((hash << 5) - hash);
+  }
+  return hash;
+}
