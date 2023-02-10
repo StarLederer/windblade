24cc51e794c2f40bae97136e96cedb413ebaa542
Herman Lederer <germans.lederers@gmail.com>
2023-02-10 13:00:22 +0100

    Fix badly named internal css variables

--- unocss/src/rules/colors.ts
+++ unocss/src/rules/colors.ts
@@ -10,7 +10,7 @@ const colorRule = (prefix: string, property: string): DynamicRule<Theme> => {
       }
 
       if (theme.windblade.colors[match[2]]) return {
-        [property]: `var(--${match[2]}-0)`
+        [property]: `var(--${match[2]}-base)`
       };
 
       return;
@@ -32,12 +32,12 @@ const colorBgRule = (prefix: string): DynamicRule<Theme> => {
       if (!color) return;
 
       const css: any = {
-        'background': `var(--${match[2]}-0)`,
-        'color': 'var(--fg-1)', // this CSS varaible gets defined below
+        'background': `var(--${match[2]}-base)`,
+        'color': `var(--${match[2]}-fg-1)`,
       };
 
       for (let i = 1; i <= color.on.length; ++i) {
-        css[`--fg-${i}`] = `var(--${match[2]}-${i})`;
+        css[`--fg-${i}`] = `var(--${match[2]}-fg-${i})`;
       }
 
       return css;
