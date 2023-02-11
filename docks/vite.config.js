7672c5708906e486618d214a555bf1bb087c093f
Herman Lederer <germans.lederers@gmail.com>
2023-02-11 15:28:58 +0100

    Remove unit overrides

--- docks/vite.config.js
+++ docks/vite.config.js
@@ -44,30 +44,6 @@ export default defineConfig({
       transformers: [
         transformerDirective(),
       ],
-      extendTheme: [
-        ({ windblade }) => {
-          windblade.proportions = {
-            's.2': 0.2,
-            's.4': 0.4,
-            's.5': 0.5,
-            's.6': 0.6,
-            's.8': 0.8,
-            's': 1,
-            'm.2': 2,
-            'm.4': 4,
-            'm.5': 5,
-            'm.6': 6,
-            'm.8': 8,
-            'm': 10,
-            'l.2': 20,
-            'l.4': 40,
-            'l.5': 50,
-            'l.6': 60,
-            'l.8': 80,
-            'l': 100,
-          };
-        }
-      ],
       safelist: [
         ...["scheme-light", "scheme-dark"].flatMap((v1) => [240, 260].map((v2) => `${v1}-${v2}`)),
       ],
