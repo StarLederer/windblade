531d7c9acd84cf508af14ed59f18cd4b97e02c74
Herman Lederer <germans.lederers@gmail.com>
2023-02-17 14:26:45 +0100

    Update example

--- unocss/src/docs/usage/dollarSyntax.ts
+++ unocss/src/docs/usage/dollarSyntax.ts
@@ -10,7 +10,7 @@ const main = `<div class="p-s">
 const colors: DocumentedThemeObject = (_, { h1, p, pre }) => [
   h1("Using the $ syntax"),
   p("You can grab relevant to the rule theme values prefixing them with $. This is especially useful inside brackets when you want to set custom values."),
-  pre(`<div class="grid grid-template-cols-[auto_$l_auto]"></div>`, 'xml'),
+  pre(`<div class="grid grid-cols-[auto_$l_auto]"></div>\n<!-- This is a bad example because at the moment brackets have limited support and do not work in this case -->`, 'xml'),
   p("You can also use the $ syntax to conveniently perform operations with your design tokens without breaking out of your design system or using calc()."),
   pre(main, 'xml'),
 ];
