aecee2edfb9aba9f9bc937d1bfad54e49c55ac84
Herman Lederer <germans.lederers@gmail.com>
2023-02-17 14:07:06 +0100

    Add logical property visualizer

--- unocss/src/docs/usage/logicalProperties.ts
+++ unocss/src/docs/usage/logicalProperties.ts
@@ -1,6 +1,10 @@
 import { DocumentedThemeObject } from "../../docs/types";
 
-const colors: DocumentedThemeObject = (_, { h1, h2, p, ul, pre }) => [
+const styles = {
+  block: "bg-surface p-s transition text-center rounded-s.4",
+};
+
+const colors: DocumentedThemeObject = (_, { h1, p, ul, example }) => [
   h1("Using logical properties"),
   p("Windblade uses logical properties and values only."),
   p("All properties that can be customized on multiple axis/edges/corenrs can be appended with:"),
@@ -11,6 +15,21 @@ const colors: DocumentedThemeObject = (_, { h1, h2, p, ul, pre }) => [
     "-is and -ie for inline start and end edges.",
     "-ss -se -es -ee for corners (start start, start end, end start & end end)",
   ]),
+  example(`
+    <div class="grid grid-cols-3 grid-auto-rows-m.2 gap-s.2 rounded-s overflow-hidden">
+      <div class="${styles.block}">ss</div>
+      <div class="${styles.block}">bs</div>
+      <div class="${styles.block}">se</div>
+
+      <div class="${styles.block}">is</div>
+      <div class="${styles.block}">center</div>
+      <div class="${styles.block}">ie</div>
+
+      <div class="${styles.block}">es</div>
+      <div class="${styles.block}">be</div>
+      <div class="${styles.block}">ee</div>
+    </div>
+  `),
   p("Windblade polyfills logical values so you can use this even where CSS does not support it yet (e.g. background-position with bg-{corner} utility)."),
   p("If you are new to logical properties try playing with bg-gradient-to-{edge/corner} and see which way the gradient goes."),
   p("Please note that width and height are completely removed in favor of size-{axis}."),
