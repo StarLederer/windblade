97fcd2ba4ff7272c8a68cd29e56ba0ab0765b302
Herman Lederer <germans.lederers@gmail.com>
2023-02-22 14:34:49 +0100

    Upodate doc page about variants

--- unocss/src/docs/usage/variants.ts
+++ unocss/src/docs/usage/variants.ts
@@ -3,7 +3,7 @@ import { DocumentedThemeObject } from "../../docs/types";
 const colors: DocumentedThemeObject<{}> = () =>
 `# Hover, focus and other states
 
-Windblade does not come with functionality like hover or focus states. Please use Windblade together with unocss-preset-mini-variants (see \"installation\") if you need this functionality.
+Windblade does not come with functionality like hover or focus states. Please use Windblade together with [unocss-preset-mini-variants](https://www.npmjs.com/package/unocss-preset-mini-variants) or other variant implementations if you need this functionality.
 `;
 
 export default colors;
