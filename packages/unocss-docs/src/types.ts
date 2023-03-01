1a48968a87ab6aa19d476fda6b55e0f54ee75bc9
Herman Lederer <germans.lederers@gmail.com>
2023-03-01 19:54:36 +0100

    Change doc types

--- packages/unocss-docs/src/types.ts
+++ packages/unocss-docs/src/types.ts
@@ -1,24 +1,10 @@
-import type { Rule } from '@unocss/core'
+export type Md = string
 
-// Theme
+export type DocumentationPage = Md
 
-export type DocumentedThemeObject<Theme extends {} = {}> = (theme: Theme) => string
+export type DocumentationTree = Map<string, DocumentationPage | DocumentationTree>
 
-// Rules
-
-export interface DocumentedRuleGroupDocs {
-  description: string
-  utilities: string[]
-  preview?: (utility: string) => string
-}
-
-export interface DocumentedRuleGroup<Theme extends {} = {}> {
-  rules: Rule<Theme>[]
-  docs: DocumentedRuleGroupDocs
-}
-
-// Categories
-
-export type DocumentationCategory<Theme extends {} = {}> = Map<string, DocumentedThemeObject<Theme> | DocumentedRuleGroup<Theme>>
-
-export type DocumentationCategories<Theme extends {} = {}> = Map<string, DocumentationCategory<Theme>>
+export type CompiledDocumentationTree = {
+  name: string
+  value: DocumentationPage | CompiledDocumentationTree
+}[]
