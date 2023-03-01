0d873e7f3209dac6a2f03b9c64e7cae7b4349367
Herman Lederer <germans.lederers@gmail.com>
2023-03-01 13:10:54 +0100

    Remove compiler from unocss-docs

--- packages/unocss-docs/src/index.ts
+++ packages/unocss-docs/src/index.ts
@@ -1,2 +1,1 @@
-export * from "./types";
-export * as compiler from "./compiler";
+export * from './types'
--- packages/unocss-docs/src/types.ts
+++ packages/unocss-docs/src/types.ts
@@ -1,24 +1,24 @@
-import { Rule } from "@unocss/core";
+import type { Rule } from '@unocss/core'
 
 // Theme
 
-export type DocumentedThemeObject<Theme extends {} = {}> = (theme: Theme) => string;
+export type DocumentedThemeObject<Theme extends {} = {}> = (theme: Theme) => string
 
 // Rules
 
-export type DocumentedRuleGroupDocs = {
-  description: string;
-  utilities: string[];
-  preview?: (utility: string) => string;
-};
+export interface DocumentedRuleGroupDocs {
+  description: string
+  utilities: string[]
+  preview?: (utility: string) => string
+}
 
-export type DocumentedRuleGroup<Theme extends {} = {}> = {
-  rules: Rule<Theme>[];
-  docs: DocumentedRuleGroupDocs;
-};
+export interface DocumentedRuleGroup<Theme extends {} = {}> {
+  rules: Rule<Theme>[]
+  docs: DocumentedRuleGroupDocs
+}
 
 // Categories
 
-export type DocumentationCategory<Theme extends {} = {}> = Map<string, DocumentedThemeObject<Theme> | DocumentedRuleGroup<Theme>>;
+export type DocumentationCategory<Theme extends {} = {}> = Map<string, DocumentedThemeObject<Theme> | DocumentedRuleGroup<Theme>>
 
-export type DocumentationCategories<Theme extends {} = {}> =  Map<string, DocumentationCategory<Theme>>
+export type DocumentationCategories<Theme extends {} = {}> = Map<string, DocumentationCategory<Theme>>
