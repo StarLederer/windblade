d183e32fbd73468dd88b09b4955c0b252e42ee9f
Herman Lederer <germans.lederers@gmail.com>
2023-03-05 00:37:13 +0100

    Add xml encode helper to unocss-docs

--- packages/unocss-docs/src/index.ts
+++ packages/unocss-docs/src/index.ts
@@ -1,2 +1,9 @@
+export const encodeString = (str: string) => str.replace(/&/g, '&amp;')
+  .replace(/</g, '&lt;')
+  .replace(/>/g, '&gt;')
+  .replace(/"/g, '&quot;')
+  .replace(/'/g, '&apos;')
+  .trimStart()
+
 export * as compiler from './compiler'
 export * from './types'
