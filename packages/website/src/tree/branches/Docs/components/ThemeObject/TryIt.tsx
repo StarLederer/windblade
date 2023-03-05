a4d4d96a915c4ce996740a61651c1af54bec98e1
Herman Lederer <germans.lederers@gmail.com>
2023-03-05 13:43:15 +0100

    Improve code consistency

--- packages/website/src/tree/branches/Docs/components/ThemeObject/TryIt.tsx
+++ packages/website/src/tree/branches/Docs/components/ThemeObject/TryIt.tsx
@@ -7,6 +7,15 @@ import uno from '~/unocss'
 
 const { formatter, highlighter } = libs
 
+export interface SelectedUtil {
+  util: string
+  renderer: string
+}
+
+export interface Props {
+  onChange: (val: SelectedUtil) => void
+}
+
 const styles = {
   pre: 'bg-surface p-s rounded-s leading-$($s+$s.4) overflow-auto',
 }
@@ -18,13 +27,57 @@ const NothingSelected = () => (
   </div>
 )
 
-const Xml: AddonXmlComponent = (props) => {
+const Xml: AddonXmlComponent<Props & {
+  selected: SelectedUtil | null
+  html: string
+  shortCss: string
+  fullCss: string
+}> = (props) => {
   const Fallback = props.fallback ?? (() => undefined)
 
-  const [selected, setSelected] = createSignal<{
-    id: string
-    util: string
-  }>()
+  return <For each={props.children}>
+    {(node) => {
+      if (node.type === 'element') {
+        switch (node.name) {
+          case 'utils':
+            return <Controls fallback={props.fallback} onChange={props.onChange}>{node.children}</Controls>
+          case 'renderer':
+            return undefined
+          case 'viewport':
+            return <Show when={props.selected} fallback={<NothingSelected />}>
+              <Viewport
+                html={props.html}
+                css={props.fullCss}
+                class="bg-normal-2 rounded-s p-m.2 overflow-auto"
+                rootStyle="display: flex; align-items: center; justify-content: center;"
+              />
+            </Show>
+          case 'html':
+            return <Show when={props.selected} fallback={<NothingSelected />} keyed>
+              {({ util }) => <pre
+                class={styles.pre}
+                innerHTML={highlighter()?.highlight(formatter()?.html_beautify(props.html) ?? '', { language: 'xml' }).value.replaceAll(util, `<span class="bg-accent-2 rounded-s.4 p-i-s.2">${util}</span>`)}
+              />}
+
+            </Show>
+          case 'css':
+            return <Show when={props.selected} fallback={<NothingSelected />}>
+              <pre
+                class={`${styles.pre} css`}
+                innerHTML={highlighter()?.highlight(formatter()?.css_beautify(props.shortCss) ?? '', { language: 'css' }).value}
+              />
+            </Show>
+        }
+      }
+
+      return <Fallback>{[node]}</Fallback>
+    }}
+  </For>
+}
+
+const main: AddonXmlComponent = (props) => {
+  const [selected, setSelected] = createSignal<SelectedUtil>()
+
   const [html, setHtml] = createSignal<string>('')
   const [css, setCss] = createSignal<{ shortCss: string; fullCss: string }>()
 
@@ -51,58 +104,30 @@ const Xml: AddonXmlComponent = (props) => {
     return [new RegExp(child.attributes.for ?? ''), child.attributes.html]
   }).filter(Boolean) as [RegExp, string][]
 
-  const selectUtil = (val: { util: string; id: string }) => {
+  const selectUtil = (val: SelectedUtil) => {
     setSelected(val)
 
     for (let i = 0; i < renderers().length; ++i) {
       const [matcher, output] = renderers()[i]
-      if (matcher.test(val.id)) {
+      if (matcher.test(val.renderer)) {
         setHtml(output)
         return
       }
     }
   }
 
-  return <For each={props.children}>
-    {(node) => {
-      if (node.type === 'element') {
-        switch (node.name) {
-          case 'utils':
-            return <Controls fallback={props.fallback} onChange={selectUtil}>{node.children}</Controls>
-          case 'renderer':
-            return undefined
-          case 'viewport':
-            return <Show when={selected()} fallback={<NothingSelected />}>
-              <Viewport
-                html={html()}
-                css={css()?.fullCss ?? ''}
-                class="bg-normal-2 rounded-s p-m.2 overflow-auto"
-                rootStyle="display: flex; align-items: center; justify-content: center;"
-              />
-            </Show>
-          case 'html':
-            return <Show when={selected()} fallback={<NothingSelected />} keyed>
-              {({ util }) => <pre
-                class={styles.pre}
-                innerHTML={highlighter()?.highlight(formatter()?.html_beautify(html()) ?? '', { language: 'xml' }).value.replaceAll(util, `<span class="bg-accent-2 rounded-s.4 p-i-s.2">${util}</span>`)}
-              />}
-
-            </Show>
-          case 'css':
-            return <Show when={selected()} fallback={<NothingSelected />}>
-              <pre
-                class={`${styles.pre} css`}
-                innerHTML={highlighter()?.highlight(formatter()?.css_beautify(css()?.shortCss ?? '') ?? '', { language: 'css' }).value}
-              />
-            </Show>
-        }
-      }
-
-      return <Fallback>{[node]}</Fallback>
-    }}
-  </For>
+  return (
+    <Xml
+      onChange={selectUtil}
+      html={html()}
+      shortCss={css()?.shortCss ?? ''}
+      fullCss={css()?.fullCss ?? ''}
+      fallback={props.fallback}
+      selected={selected() ?? null}
+    >
+      {props.children}
+    </Xml>
+  )
 }
-
-const main: AddonXmlComponent = props => <Xml fallback={props.fallback}>{props.children}</Xml>
 
 export default main
