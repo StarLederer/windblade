a1409d21e8b79bf0f3f77b4104bf6646277863be
Herman Lederer <germans.lederers@gmail.com>
2023-03-06 18:09:45 +0100

    Fix 'for' resetting variable context

--- packages/website/src/tree/branches/Docs/components/ThemeObject/For.tsx
+++ packages/website/src/tree/branches/Docs/components/ThemeObject/For.tsx
@@ -1,4 +1,4 @@
-import { For } from 'solid-js'
+import { For, useContext } from 'solid-js'
 import type { AddonXmlComponent } from './XmlComponent'
 import VariableContext, { applyVars } from './XmlVariables'
 import uno from '~/unocss'
@@ -24,36 +24,41 @@ interface IterateArrayProps {
 
 type Props = IterateObjectProps | IterateArrayProps
 
-const main: AddonXmlComponent<Props> = props => <>{(() => {
-  switch (props.type) {
-    case 'array':
-      return (
-        <For each={navigateUnoConfig(applyVars(props.array))}>
-          {value => (
-            <VariableContext.Provider value={{ value }}>
-              <props.fallback>
-                {props.children}
-              </props.fallback>
-            </VariableContext.Provider>
-          )}
-        </For>
-      )
-    case 'object':
-      return (
-        <For each={Object.entries(navigateUnoConfig(applyVars(props.object)) ?? {})}>
-          {([key, value]) => (
-            <VariableContext.Provider value={{
-              [props.keyAs]: key,
-              [props.valueAs]: `${value}`,
-            }}>
-              <props.fallback>
-                {props.children}
-              </props.fallback>
-            </VariableContext.Provider>
-          )}
-        </For>
-      )
-  }
-})()}</>
+const main: AddonXmlComponent<Props> = (props) => {
+  const parentContext = useContext(VariableContext)
+
+  return <>{(() => {
+    switch (props.type) {
+      case 'array':
+        return (
+          <For each={navigateUnoConfig(applyVars(props.array))}>
+            {value => (
+              <VariableContext.Provider value={{ ...parentContext, value }}>
+                <props.fallback>
+                  {props.children}
+                </props.fallback>
+              </VariableContext.Provider>
+            )}
+          </For>
+        )
+      case 'object':
+        return (
+          <For each={Object.entries(navigateUnoConfig(applyVars(props.object)) ?? {})}>
+            {([key, value]) => (
+              <VariableContext.Provider value={{
+                ...parentContext,
+                [props.keyAs]: key,
+                [props.valueAs]: `${value}`,
+              }}>
+                <props.fallback>
+                  {props.children}
+                </props.fallback>
+              </VariableContext.Provider>
+            )}
+          </For>
+        )
+    }
+  })()}</>
+}
 
 export default main
