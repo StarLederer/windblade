8e625fb258d8e1feb6a936996b5a554945dc403e
Herman Lederer <germans.lederers@gmail.com>
2023-03-06 17:46:11 +0100

    Implement applyVars function

--- packages/website/src/tree/branches/Docs/components/ThemeObject/XmlVariables.ts
+++ packages/website/src/tree/branches/Docs/components/ThemeObject/XmlVariables.ts
@@ -1,5 +1,14 @@
-import { createContext } from 'solid-js'
+import { createContext, useContext } from 'solid-js'
 
-const main = createContext<Record<string, unknown>>()
+const main = createContext<Record<string, string>>()
+
+export const applyVars = (input: string): string => {
+  let output = input
+  const context = useContext(main) ?? {}
+  Object.entries(context).forEach(([key, value]) => {
+    output = output.replaceAll(key, value)
+  })
+  return input
+}
 
 export default main
