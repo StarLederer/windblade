8405c2a3e0798fed52a42ab45a19b6c74e8dddc5
Herman Lederer <germans.lederers@gmail.com>
2023-03-08 19:08:03 +0100

    Update color/typography docs to xml

--- packages/unocss-preset-color/src/rules/documented/typography.ts
+++ packages/unocss-preset-color/src/rules/documented/typography.ts
@@ -1,44 +1,107 @@
 import type { Rule } from '@unocss/core'
-import type { DocumentedRuleGroup, DocumentedRuleGroupDocs } from 'unocss-docs'
+import type { DocumentationPage } from 'unocss-docs'
+import { encodeString } from 'unocss-docs'
 import { ruleUtils } from '@windblade/core'
 import type { theme } from '@windblade/core'
 
 const { color } = ruleUtils
 
-export const textColor = (): DocumentedRuleGroup<theme.Theme> => {
+export const textColor = () => {
   const rules: Rule<theme.Theme>[] = [
     color.colorRule('text', 'color'),
     color.fgColorRule('text-fg', 'color'),
   ]
 
-  const docs: DocumentedRuleGroupDocs = {
-    description: 'Windblade uses semantic colors.',
-    utilities: [
-      'text-<theme.windblade.colors>',
-      'text-<theme.windblade.miscColors>',
-      'text-fg-<integer>',
-    ],
-    preview: util => `<div class="${util}">Lorem ipsum<div>`,
-  }
+  const docs: DocumentationPage = `
+    <page>
+      <h1><title /></h1>
+      <p>Windblade uses semantic colors.</p>
+
+      <h2>Try it</h2>
+      <try-it selected="$util">
+        <utils>
+          <util>
+            text-
+            <select>
+              <for object="theme.windblade.colors" key-as="$name" value-as="$color">
+                <option value="$name"/>
+              </for>
+              <for object="theme.windblade.miscColors" key-as="$name" value-as="$color">
+                <option value="$name"/>
+              </for>
+            </select>
+          </util>
+          <util renderer="fg">
+            text-fg-
+            <input type="integer" />
+          </util>
+        </utils>
+
+        <renderer html="${encodeString(`
+          <div class="$util">Lorem ipsum<div>
+        `)}" />
+
+        <h3>Preview</h3>
+        <viewport />
+
+        <h3>HTML</h3>
+        <html />
+
+        <h3>Generated CSS</h3>
+        <css />
+      </try-it>
+    </page>
+  `
 
   return { rules, docs }
 }
 
-export const textDecorationColor = (): DocumentedRuleGroup<theme.Theme> => {
+export const textDecorationColor = () => {
   const rules: Rule<theme.Theme>[] = [
     color.colorRule('decoration', 'text-decoration-color'),
     color.fgColorRule('decoration-fg', 'text-decoration-color'),
   ]
 
-  const docs: DocumentedRuleGroupDocs = {
-    description: 'Windblade uses semantic colors.',
-    utilities: [
-      'decoration-<theme.windblade.colors>',
-      'decoration-<theme.windblade.miscColors>',
-      'decoration-fg-<integer>',
-    ],
-    preview: util => `<div class="underline ${util}">Lorem ipsum<div>`,
-  }
+  const docs: DocumentationPage = `
+    <page>
+      <h1><title /></h1>
+      <p>Windblade uses semantic colors.</p>
+
+      <h2>Try it</h2>
+      <try-it selected="$util">
+        <utils>
+          <util>
+          decoration-
+            <select>
+              <for object="theme.windblade.colors" key-as="$name" value-as="$color">
+                <option value="$name"/>
+              </for>
+              <for object="theme.windblade.miscColors" key-as="$name" value-as="$color">
+                <option value="$name"/>
+              </for>
+            </select>
+          </util>
+          <util renderer="fg">
+          decoration-fg-
+            <input type="integer" />
+          </util>
+        </utils>
+
+        <renderer html="${encodeString(`
+          <div class="underline $util">Lorem ipsum<div>
+        `)}" />
+
+        <h3>Preview</h3>
+        <viewport />
+
+        <h3>HTML</h3>
+        <html />
+
+        <h3>Generated CSS</h3>
+        <css />
+      </try-it>
+    </page>
+  `
 
   return { rules, docs }
 }
