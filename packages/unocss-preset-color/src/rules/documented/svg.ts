520cea003e2243bbc95ea7a5e6cadb6d054e58e5
Herman Lederer <germans.lederers@gmail.com>
2023-03-08 19:29:36 +0100

    Update color/svg docs to xml

--- packages/unocss-preset-color/src/rules/documented/svg.ts
+++ packages/unocss-preset-color/src/rules/documented/svg.ts
@@ -1,44 +1,107 @@
 import type { Rule } from '@unocss/core'
-import type { DocumentedRuleGroup, DocumentedRuleGroupDocs } from 'unocss-docs'
+import type { DocumentationPage } from 'unocss-docs'
+import { encodeString } from 'unocss-docs'
 import { ruleUtils } from '@windblade/core'
 import type { theme } from '@windblade/core'
 
 const { color } = ruleUtils
 
-export const fill = (): DocumentedRuleGroup<theme.Theme> => {
+export const fill = () => {
   const rules: Rule<theme.Theme>[] = [
     color.colorRule('fill', 'fill'),
     color.fgColorRule('fill-fg', 'fill'),
   ]
 
-  const docs: DocumentedRuleGroupDocs = {
-    description: 'Windblade uses semantic colors.',
-    utilities: [
-      'fill-<theme.windblade.colors>',
-      'fill-<theme.windblade.miscColors>',
-      'fill-fg-<integer>',
-    ],
-    preview: util => 'TODO',
-  }
+  const docs: DocumentationPage = `
+    <page>
+      <h1><title /></h1>
+      <p>Windblade uses semantic colors.</p>
+
+      <h2>Try it</h2>
+      <try-it selected="$util">
+        <utils>
+          <util>
+          fill-
+            <select>
+              <for object="theme.windblade.colors" key-as="$name" value-as="$color">
+                <option value="$name"/>
+              </for>
+              <for object="theme.windblade.miscColors" key-as="$name" value-as="$color">
+                <option value="$name"/>
+              </for>
+            </select>
+          </util>
+          <util renderer="fg">
+          fill-fg-
+            <input type="integer" />
+          </util>
+        </utils>
+
+        <renderer html="${encodeString(`
+          TODO
+        `)}" />
+
+        <h3>Preview</h3>
+        <viewport />
+
+        <h3>HTML</h3>
+        <html />
+
+        <h3>Generated CSS</h3>
+        <css />
+      </try-it>
+    </page>
+  `
 
   return { rules, docs }
 }
 
-export const stroke = (): DocumentedRuleGroup<theme.Theme> => {
+export const stroke = () => {
   const rules: Rule<theme.Theme>[] = [
     color.colorRule('stroke', 'stroke'),
     color.fgColorRule('stroke-fg', 'stroke'),
   ]
 
-  const docs: DocumentedRuleGroupDocs = {
-    description: 'Windblade uses semantic colors.',
-    utilities: [
-      'stroke-<theme.windblade.colors>',
-      'stroke-<theme.windblade.miscColors>',
-      'stroke-fg-<integer>',
-    ],
-    preview: util => 'TODO',
-  }
+  const docs: DocumentationPage = `
+    <page>
+      <h1><title /></h1>
+      <p>Windblade uses semantic colors.</p>
+
+      <h2>Try it</h2>
+      <try-it selected="$util">
+        <utils>
+          <util>
+          stroke-
+            <select>
+              <for object="theme.windblade.colors" key-as="$name" value-as="$color">
+                <option value="$name"/>
+              </for>
+              <for object="theme.windblade.miscColors" key-as="$name" value-as="$color">
+                <option value="$name"/>
+              </for>
+            </select>
+          </util>
+          <util renderer="fg">
+          stroke-fg-
+            <input type="integer" />
+          </util>
+        </utils>
+
+        <renderer html="${encodeString(`
+          TODO
+        `)}" />
+
+        <h3>Preview</h3>
+        <viewport />
+
+        <h3>HTML</h3>
+        <html />
+
+        <h3>Generated CSS</h3>
+        <css />
+      </try-it>
+    </page>
+  `
 
   return { rules, docs }
 }
