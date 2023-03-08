227aacc712425cffef97560a5cc8c4b22ca0a008
Herman Lederer <germans.lederers@gmail.com>
2023-03-08 20:58:47 +0100

    Fix type issues

--- packages/unocss-preset/src/docs/index.ts
+++ packages/unocss-preset/src/docs/index.ts
@@ -1,10 +1,9 @@
-import type { DocumentationCategories } from 'unocss-docs'
-import type { theme as coreTheme } from '@windblade/core'
+import type { DocumentationTree } from 'unocss-docs'
 import usage from './usage'
 import theme from './theme'
 import rules from './rules'
 
-const main: DocumentationCategories<coreTheme.Theme> = new Map([
+const main: DocumentationTree = new Map([
   ['Usage', usage],
   ['Theme', theme],
   ...rules,
