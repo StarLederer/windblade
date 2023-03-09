3b6000562b479875e20ffbbfd326ee8cf3806976
Herman Lederer <germans.lederers@gmail.com>
2023-03-09 15:16:39 +0100

    Update sizing docs to xml

--- packages/unocss-preset/src/rules/documented/sizing.ts
+++ packages/unocss-preset/src/rules/documented/sizing.ts
@@ -1,15 +1,20 @@
 import type { Rule } from '@unocss/core'
 import type { DocumentationPage } from 'unocss-docs'
+import { encodeString } from 'unocss-docs'
 import { ruleUtils } from '@windblade/core'
 import type { theme } from '@windblade/core'
+import { objectKeys } from 'ts-extras'
+import { iterObjects } from './doc-components'
 
 const { logical, size: sizes } = ruleUtils
 
 export const widthHeight = () => {
-  const docs: DocumentationPage = {
-    description: 'Removed this, as well as min and max variants, in favor of the size counterparts',
-    utilities: [],
-  }
+  const docs: DocumentationPage = `
+    <page>
+      <h1><title /></h1>
+      <p>Removed this, as well as min and max variants, in favor of the size counterparts</p>
+    </page>
+  `
 
   return { rules: [], docs }
 }
@@ -17,31 +22,91 @@ export const widthHeight = () => {
 export const size = () => {
   const rules: Rule<theme.Theme>[] = sizes.axisRules('size', '', '', 'size')
 
-  const docs: DocumentationPage = {
-    description: 'Utilities for setting the size of an element. Missing from Tailwind.',
-    utilities: [
-      ...Object.keys(logical.abbreviations.axis).map(val => `size-${val}-<theme.windblade.sizes>`),
-    ],
-    preview: util => `
-      <div class="${util} ${util.includes('-i-') ? 'min-size-b-m.2' : 'min-size-i-m.2'} bg-accent rounded-s"></div>
-    `,
-  }
+  const docs: DocumentationPage = `
+    <page>
+      <h1><title /></h1>
+      <p>Utilities for setting the size of an element. Missing from Tailwind.</p>
+
+      <h2>Try it</h2>
+      <try-it selected="$util">
+        <utils>
+          ${objectKeys(logical.abbreviations.axis).map(axis => `
+            <util renderer="${axis}">
+              size-${axis}-
+              <select>
+                ${iterObjects(['theme.windblade.proportions', 'theme.windblade.miscSizes'], '$name', '$val', `
+                  <option value="$name"/>
+                `)}
+              </select>
+            </util>
+          `).join('')}
+        </utils>
+
+        ${objectKeys(logical.abbreviations.axis).map(axis => `
+          <renderer for="${axis}" html="${encodeString(`
+            <div class="$util ${axis === 'i' ? 'min-size-b-m.2' : 'min-size-i-m.2'} bg-accent rounded-s"></div>
+          `)}" />
+        `).join('')}
+
+        <h3>Preview</h3>
+        <viewport />
+
+        <h3>HTML</h3>
+        <html />
+
+        <h3>Generated CSS</h3>
+        <css />
+      </try-it>
+    </page>
+  `
 
   return { rules, docs }
 }
 
-const minMaxSizePreview = (util: string) => `
-  <div class="${util} ${util.includes('-i-') ? 'min-size-b-m.2' : 'min-size-i-m.2'} bg-accent rounded-s"></div>
+const minMaxSizeHtml = (axis: string) => `
+  <div class="$util ${axis === 'inline' ? 'min-size-b-m.2' : 'min-size-i-m.2'} bg-accent rounded-s"></div>
 `
 
 export const minSize = () => {
   const rules: Rule<theme.Theme>[] = sizes.axisRules('min-size', '', 'min', 'size')
 
-  const docs: DocumentationPage = {
-    description: 'Utilities for setting the minimum size of an element. Missing from Tailwind.',
-    utilities: Object.keys(logical.abbreviations.axis).map(val => `min-size-${val}-<theme.windblade.proportions>`),
-    preview: minMaxSizePreview,
-  }
+  const docs: DocumentationPage = `
+    <page>
+      <h1><title /></h1>
+      <p>Utilities for setting the minimum size of an element. Missing from Tailwind.</p>
+
+      <h2>Try it</h2>
+      <try-it selected="$util">
+        <utils>
+          ${objectKeys(logical.abbreviations.axis).map(axis => `
+            <util renderer="${axis}">
+              min-size-${axis}-
+              <select>
+                ${iterObjects(['theme.windblade.proportions', 'theme.windblade.miscSizes'], '$name', '$val', `
+                  <option value="$name" />
+                `)}
+              </select>
+            </util>
+          `).join('')}
+        </utils>
+
+        ${objectKeys(logical.abbreviations.axis).map(axis => `
+          <renderer for="${axis}" html="${encodeString(`
+            ${minMaxSizeHtml(axis)}
+          `)}" />
+        `).join('')}
+
+        <h3>Preview</h3>
+        <viewport />
+
+        <h3>HTML</h3>
+        <html />
+
+        <h3>Generated CSS</h3>
+        <css />
+      </try-it>
+    </page>
+  `
 
   return { rules, docs }
 }
@@ -49,11 +114,43 @@ export const minSize = () => {
 export const maxSize = () => {
   const rules: Rule<theme.Theme>[] = sizes.axisRules('max-size', '', 'max', 'size')
 
-  const docs: DocumentationPage = {
-    description: 'Utilities for setting the maximum size of an element. Missing from Tailwind.',
-    utilities: Object.keys(logical.abbreviations.axis).map(val => `max-size-${val}-<theme.windblade.proportions>`),
-    preview: minMaxSizePreview,
-  }
+  const docs: DocumentationPage = `
+    <page>
+      <h1><title /></h1>
+      <p>Utilities for setting the maximum size of an element. Missing from Tailwind.</p>
+
+      <h2>Try it</h2>
+      <try-it selected="$util">
+        <utils>
+          ${objectKeys(logical.abbreviations.axis).map(axis => `
+            <util renderer="${axis}">
+              max-size-${axis}-
+              <select>
+                ${iterObjects(['theme.windblade.proportions', 'theme.windblade.miscSizes'], '$name', '$val', `
+                  <option value="$name"/>
+                `)}
+              </select>
+            </util>
+          `).join('')}
+        </utils>
+
+        ${objectKeys(logical.abbreviations.axis).map(axis => `
+          <renderer for="${axis}" html="${encodeString(`
+            ${minMaxSizeHtml(axis)}
+          `)}" />
+        `).join('')}
+
+        <h3>Preview</h3>
+        <viewport />
+
+        <h3>HTML</h3>
+        <html />
+
+        <h3>Generated CSS</h3>
+        <css />
+      </try-it>
+    </page>
+  `
 
   return { rules, docs }
 }
