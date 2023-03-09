502df7d2379b3308b9aab6875f560bf2ffebd703
Herman Lederer <germans.lederers@gmail.com>
2023-03-09 15:16:49 +0100

    Update spacing docs to xml

--- packages/unocss-preset/src/rules/documented/spacing.ts
+++ packages/unocss-preset/src/rules/documented/spacing.ts
@@ -1,26 +1,59 @@
 import type { Rule } from '@unocss/core'
 import type { DocumentationPage } from 'unocss-docs'
+import { encodeString } from 'unocss-docs'
 import { ruleUtils } from '@windblade/core'
 import type { theme } from '@windblade/core'
+import { iterObjects, selectLogical } from './doc-components'
 
 const { logical, size } = ruleUtils
 
 export const padding = () => {
   const rules: Rule<theme.Theme>[] = size.edgeRules('p', '', 'padding', '')
 
-  const docs: DocumentationPage = {
-    description: 'Replaced physical properties with logical.',
-    utilities: [
-      'p',
-      ...Object.keys(logical.abbreviations.axis).map(val => `p-${val}`),
-      ...Object.keys(logical.abbreviations.edges).map(val => `p-${val}`),
-    ].map(val => `${val}-<theme.windblade.proportions>`),
-    preview: util => `
-      <div class="${util} rounded-s bg-accent">
-        <div class="border border-dashed rounded-s.2">${util}</div>
-      </div>
-    `,
-  }
+  const docs: DocumentationPage = `
+    <page>
+      <h1><title /></h1>
+      <p>Replaced physical properties with logical.</p>
+
+      <h2>Try it</h2>
+      <try-it selected="$util">
+        <utils>
+          <util>
+            p-
+            <select>
+              ${iterObjects(['theme.windblade.proportions', 'theme.windblade.miscSizes'], '$name', '$val', `
+                <option value="$name"/>
+              `)}
+            </select>
+          </util>
+          <util>
+            p-
+            ${selectLogical({ axis: true, edges: true })}-
+            <select>
+              ${iterObjects(['theme.windblade.proportions', 'theme.windblade.miscSizes'], '$name', '$val', `
+                <option value="$name"/>
+              `)}
+            </select>
+          </util>
+        </utils>
+
+        <renderer html="${encodeString(`
+          <div class="$util rounded-s bg-accent">
+            <div class="border border-dashed rounded-s.2">Padding</div>
+          </div>
+        `)}" />
+
+        <h3>Preview</h3>
+        <viewport />
+
+        <h3>HTML</h3>
+        <html />
+
+        <h3>Generated CSS</h3>
+        <css />
+      </try-it>
+    </page>
+  `
 
   return { rules, docs }
 }
@@ -28,28 +61,61 @@ export const padding = () => {
 export const margin = () => {
   const rules: Rule<theme.Theme>[] = size.edgeRules('m', '', 'margin', '')
 
-  const docs: DocumentationPage = {
-    description: 'Replaced physical properties with logical.',
-    utilities: [
-      'm',
-      ...Object.keys(logical.abbreviations.axis).map(val => `m-${val}`),
-      ...Object.keys(logical.abbreviations.edges).map(val => `m-${val}`),
-    ].map(val => `${val}-<theme.windblade.proportions>`),
-    preview: util => `
-      <div class="border border-dashed border-color-accent rounded-s.2">
-        <div class="${util} p-s rounded-s bg-accent">${util}</div>
-      </div>
-    `,
-  }
+  const docs: DocumentationPage = `
+    <page>
+      <h1><title /></h1>
+      <p>Replaced physical properties with logical.</p>
+
+      <h2>Try it</h2>
+      <try-it selected="$util">
+        <utils>
+          <util>
+            m-
+            <select>
+              ${iterObjects(['theme.windblade.proportions', 'theme.windblade.miscSizes'], '$name', '$val', `
+                <option value="$name"/>
+              `)}
+            </select>
+          </util>
+          <util>
+            m-
+            ${selectLogical({ axis: true, edges: true })}-
+            <select>
+              ${iterObjects(['theme.windblade.proportions', 'theme.windblade.miscSizes'], '$name', '$val', `
+                <option value="$name"/>
+              `)}
+            </select>
+          </util>
+        </utils>
+
+        <renderer html="${encodeString(`
+          <div class="border border-dashed border-color-accent rounded-s.2">
+            <div class="$util p-s rounded-s bg-accent">Margin</div>
+          </div>
+        `)}" />
+
+        <h3>Preview</h3>
+        <viewport />
+
+        <h3>HTML</h3>
+        <html />
+
+        <h3>Generated CSS</h3>
+        <css />
+      </try-it>
+    </page>
+  `
 
   return { rules, docs }
 }
 
 export const spaceBetween = () => {
-  const docs: DocumentationPage = {
-    description: 'Removed this. Please use gap and flex/grid/columns instead',
-    utilities: [],
-  }
+  const docs: DocumentationPage = `
+    <page>
+      <h1><title /></h1>
+      <p>Removed this. Please use gap and flex/grid/columns instead.</p>
+    </page>
+  `
 
   return { rules: [], docs }
 }
