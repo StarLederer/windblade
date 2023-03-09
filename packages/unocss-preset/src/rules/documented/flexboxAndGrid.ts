d04da50754b9be4f995077744955494025462e44
Herman Lederer <germans.lederers@gmail.com>
2023-03-09 13:58:48 +0100

    Update flexbox and grid docs to xml

--- packages/unocss-preset/src/rules/documented/flexboxAndGrid.ts
+++ packages/unocss-preset/src/rules/documented/flexboxAndGrid.ts
@@ -1,9 +1,12 @@
 import type { Rule } from '@unocss/core'
 import type { DocumentationPage } from 'unocss-docs'
+import { encodeString } from 'unocss-docs'
 import { ruleUtils } from '@windblade/core'
 import type { theme } from '@windblade/core'
 
 const { size } = ruleUtils
+
+const nineChildren = [1, 2, 3, 4, 5, 6, 7, 8, 9].map(val => `<div class="bg-accent rounded-s.4 p-s flex items-center justify-center text-center">0${val}</div>`).join('\n')
 
 const generateAuto = (ruleName: string, cssName: string) => () => {
   const rules: Rule<theme.Theme>[] = [
@@ -12,23 +15,43 @@ const generateAuto = (ruleName: string, cssName: string) => () => {
     size.rule(`auto-${ruleName}`, `grid-auto-${cssName}`),
   ]
 
-  const docs: DocumentationPage = {
-    description: 'Added utilities for controlling the size of implicitly-created grid columns with proportion units.',
-    utilities: [`auto-${ruleName}-auto`, `auto-${ruleName}-fr`, `auto-${ruleName}-<theme.windblade.proportions>`],
-    preview: util => `
-      <div class="grid ${util} ${util.includes('cols') ? 'grid-flow-col' : ''} gap-s bg-accent-3 rounded-s.4 size-i-full">
-        <div class="bg-accent rounded-s.4 p-s flex items-center justify-center text-center">01</div>
-        <div class="bg-accent rounded-s.4 p-s flex items-center justify-center text-center">02</div>
-        <div class="bg-accent rounded-s.4 p-s flex items-center justify-center text-center">03</div>
-        <div class="bg-accent rounded-s.4 p-s flex items-center justify-center text-center">04</div>
-        <div class="bg-accent rounded-s.4 p-s flex items-center justify-center text-center">05</div>
-        <div class="bg-accent rounded-s.4 p-s flex items-center justify-center text-center">06</div>
-        <div class="bg-accent rounded-s.4 p-s flex items-center justify-center text-center">07</div>
-        <div class="bg-accent rounded-s.4 p-s flex items-center justify-center text-center">08</div>
-        <div class="bg-accent rounded-s.4 p-s flex items-center justify-center text-center">09</div>
-      </div>
-    `,
-  }
+  const docs: DocumentationPage = `
+    <page>
+      <h1><title /></h1>
+      <p>Added utilities for controlling the size of implicitly-created grid columns with proportion units.</p>
+
+      <h2>Try it</h2>
+      <try-it selected="$util">
+        <utils>
+          <util>auto-${ruleName}-auto</util>
+          <util>auto-${ruleName}-fr</util>
+          <util>
+            auto-${ruleName}-
+            <select>
+              <for object="theme.windblade.proportions" key-as="$name" value-as="$value">
+                <option value="$name"/>
+              </for>
+            </select>
+          </util>
+        </utils>
+
+        <renderer html="${encodeString(`
+          <div class="grid $util ${ruleName === 'cols' ? 'grid-flow-col' : 'grid-flow-row'} gap-s bg-accent-3 rounded-s.4 size-i-full">
+            ${nineChildren}
+          </div>
+        `)}" />
+
+        <h3>Preview</h3>
+        <viewport />
+
+        <h3>HTML</h3>
+        <html />
+
+        <h3>Generated CSS</h3>
+        <css />
+      </try-it>
+    </page>
+  `
 
   return { rules, docs }
 }
@@ -41,23 +64,41 @@ const generateFitFill = (type: 'fit' | 'fill', ruleName: string, cssName: string
     size.rule(`grid-${type}-${ruleName}s`, `grid-template-${cssName}s`, { postprocess: size => (`repeat(auto-fit, minmax(${size}, 1fr))`) }),
   ]
 
-  const docs: DocumentationPage = {
-    description: `Utilities specifying the columns in a grid layout using auto-${type}. Missing from Tailwind.`,
-    utilities: [`grid-${type}-${ruleName}s-<theme.windblade.proportions>`],
-    preview: util => `
-      <div class="grid ${util} ${util.includes('cols') ? '' : 'grid-flow-col'} gap-s bg-accent-3 rounded-s.4 size-i-full">
-        <div class="bg-accent rounded-s.4 p-s flex items-center justify-center text-center">01</div>
-        <div class="bg-accent rounded-s.4 p-s flex items-center justify-center text-center">02</div>
-        <div class="bg-accent rounded-s.4 p-s flex items-center justify-center text-center">03</div>
-        <div class="bg-accent rounded-s.4 p-s flex items-center justify-center text-center">04</div>
-        <div class="bg-accent rounded-s.4 p-s flex items-center justify-center text-center">05</div>
-        <div class="bg-accent rounded-s.4 p-s flex items-center justify-center text-center">06</div>
-        <div class="bg-accent rounded-s.4 p-s flex items-center justify-center text-center">07</div>
-        <div class="bg-accent rounded-s.4 p-s flex items-center justify-center text-center">08</div>
-        <div class="bg-accent rounded-s.4 p-s flex items-center justify-center text-center">09</div>
-      </div>
-    `,
-  }
+  const docs: DocumentationPage = `
+    <page>
+      <h1><title /></h1>
+      <p>Utilities specifying the columns in a grid layout using auto-${type}. Missing from Tailwind.</p>
+
+      <h2>Try it</h2>
+      <try-it selected="$util">
+        <utils>
+          <util>
+            grid-${type}-${ruleName}s-
+            <select>
+              <for object="theme.windblade.proportions" key-as="$name" value-as="$value">
+                <option value="$name"/>
+              </for>
+            </select>
+          </util>
+        </utils>
+
+        <renderer html="${encodeString(`
+          <div class="grid $util ${ruleName === 'row' ? 'grid-flow-col' : 'grid-flow-auto'} gap-s bg-accent-3 rounded-s.4 size-i-full">
+            ${nineChildren}
+          </div>
+        `)}" />
+
+        <h3>Preview</h3>
+        <viewport />
+
+        <h3>HTML</h3>
+        <html />
+
+        <h3>Generated CSS</h3>
+        <css />
+      </try-it>
+    </page>
+  `
 
   return { rules, docs }
 }
