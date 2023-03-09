babbbc0b7d3728db4a59168d00072f1de98112fd
Herman Lederer <germans.lederers@gmail.com>
2023-03-09 20:09:29 +0100

    Update background docs to xml

--- packages/unocss-preset/src/rules/documented/backgrounds.ts
+++ packages/unocss-preset/src/rules/documented/backgrounds.ts
@@ -1,5 +1,6 @@
 import type { Rule } from '@unocss/core'
 import type { DocumentationPage } from 'unocss-docs'
+import { encodeString } from 'unocss-docs'
 import { objectEntries } from 'ts-extras'
 import { ruleUtils } from '@windblade/core'
 import type { theme } from '@windblade/core'
@@ -17,17 +18,36 @@ export const backgroundPosition = () => {
     ['bg-center', { 'background-position': 'center' }],
   ]
 
-  const docs: DocumentationPage = {
-    description: 'Physical properties replaced with logocal.',
-    utilities: [
-      ...Object.keys(logical.abbreviations.edges),
-      ...Object.keys(logical.abbreviations.coners),
-      'center',
-    ].map(val => `bg-${val}`),
-    preview: util => `
-      <div class="${util} rounded-s.4 size-i-full max-size-i-l.2 aspect-10/6" style="background-image: url('https://picsum.photos/600/400')"></div>
-    `,
-  }
+  const docs: DocumentationPage = `
+    <page>
+      <h1><title /></h1>
+      <p>Physical properties replaced with logocal.</p>
+
+      <h2>Try it</h2>
+      <try-it selected="$util">
+        <utils>${[
+          ...Object.keys(logical.abbreviations.edges),
+          ...Object.keys(logical.abbreviations.coners),
+          'center',
+        ].map(val => `
+          <util>bg-${val}</util>
+        `).join('')}</utils>
+
+        <renderer html="${encodeString(`
+          <div class="$util rounded-s.4 size-i-full max-size-i-l.2 aspect-10/6" style="background-image: url('https://picsum.photos/600/400')"></div>
+        `)}" />
+
+        <h3>Preview</h3>
+        <viewport />
+
+        <h3>HTML</h3>
+        <html />
+
+        <h3>Generated CSS</h3>
+        <css />
+      </try-it>
+    </page>
+  `
 
   return { rules, docs }
 }
