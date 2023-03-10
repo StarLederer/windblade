8cf3691940b1c7843da9e869f5b0eceadeedd017
Herman Lederer <germans.lederers@gmail.com>
2023-03-10 20:34:46 +0100

    Update filter docs to xml

--- packages/unocss-preset/src/rules/documented/filters.ts
+++ packages/unocss-preset/src/rules/documented/filters.ts
@@ -1,10 +1,12 @@
 import type { DocumentationPage } from 'unocss-docs'
 
 export const dropShadow = () => {
-  const docs: DocumentationPage = {
-    description: 'Drop shadows are removed for now because Tailwind\'s implementation is too limiting. Discussion in progress.',
-    utilities: [],
-  }
+  const docs: DocumentationPage = `
+    <page>
+      <h1><title /></h1>
+      <p>Drop shadows are removed for now because Tailwind\'s implementation is too limiting. Discussion in progress.</p>
+    </page>
+  `
 
   return { rules: [], docs }
 }
