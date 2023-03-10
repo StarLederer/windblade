59166fd1629029f7bfa7c40a35df6f0ba5c672d1
Herman Lederer <germans.lederers@gmail.com>
2023-03-10 20:34:24 +0100

    Update svg docs to xml

--- packages/unocss-preset/src/rules/documented/svg.ts
+++ packages/unocss-preset/src/rules/documented/svg.ts
@@ -1,5 +1,6 @@
 import type { Rule } from '@unocss/core'
 import type { DocumentationPage } from 'unocss-docs'
+import { encodeString } from 'unocss-docs'
 import { ruleUtils } from '@windblade/core'
 import type { theme } from '@windblade/core'
 
@@ -8,11 +9,39 @@ const { size } = ruleUtils
 export const strokeWidth = () => {
   const rules: Rule<theme.Theme>[] = [size.rule('stroke', 'stroke-width')]
 
-  const docs: DocumentationPage = {
-    description: 'Windblade proportions are used instead of separate size values.',
-    utilities: ['stroke-<theme.windblade.proportions>'],
-    preview: util => 'TODO',
-  }
+  const docs: DocumentationPage = `
+    <page>
+      <h1><title /></h1>
+      <p>Windblade proportions are used instead of separate size values.</p>
+
+      <h2>Try it</h2>
+      <try-it selected="$util">
+        <utils>
+          <util>
+            stroke-
+            <select>
+              <for object="theme.windblade.proportions" key-as="$name" value-as="$value">
+                <option value="$name" />
+              </for>
+            </select>
+          </util>
+        </utils>
+
+        <renderer html="${encodeString(`
+          TODO
+        `)}" />
+
+        <h3>Preview</h3>
+        <viewport />
+
+        <h3>HTML</h3>
+        <html />
+
+        <h3>Generated CSS</h3>
+        <css />
+      </try-it>
+    </page>
+  `
 
   return { rules, docs }
 }
