fa714f9f5bf5547c52b021b8c84009fee2c2fda0
Herman Lederer <germans.lederers@gmail.com>
2023-03-10 20:35:17 +0100

    Update transform docs to xml

--- packages/unocss-preset/src/rules/documented/transforms.ts
+++ packages/unocss-preset/src/rules/documented/transforms.ts
@@ -1,5 +1,6 @@
 import type { Rule } from '@unocss/core'
 import type { DocumentationPage } from 'unocss-docs'
+import { encodeString } from 'unocss-docs'
 import { ruleUtils } from '@windblade/core'
 import type { theme } from '@windblade/core'
 
@@ -8,11 +9,39 @@ const { size } = ruleUtils
 export const scale = () => {
   const rules: Rule<theme.Theme>[] = [size.rule('scale', 'transform', { defaultUnit: '', postprocess: val => `scale${val}` })]
 
-  const docs: DocumentationPage = {
-    description: 'X and Y variants have been removed because they are not logical properties. Windblade also uses proportions instead of separete values.',
-    utilities: ['scale-<theme.windblade.proportions>'],
-    preview: util => 'TODO',
-  }
+  const docs: DocumentationPage = `
+    <page>
+      <h1><title /></h1>
+      <p>X and Y variants have been removed because they are not logical properties. Windblade also uses proportions instead of separete values.</p>
+
+      <h2>Try it</h2>
+      <try-it selected="$util">
+        <utils>
+          <util>
+            scale-
+            <select>
+              <for object="theme.windblade.proportions" key-as="$name" value-as="$value">
+                <option value="$name" />
+              </for>
+            </select>
+          </util>
+        </utils>
+
+        <renderer html="${encodeString(`
+          TODO
+        `)}" />
+
+        <h3>Preview</h3>
+        <viewport />
+
+        <h3>HTML</h3>
+        <html />
+
+        <h3>Generated CSS</h3>
+        <css />
+      </try-it>
+    </page>
+  `
 
   return { rules, docs }
 }
@@ -20,11 +49,39 @@ export const scale = () => {
 export const rotate = () => {
   const rules: Rule<theme.Theme>[] = [size.rule('rotate', 'transform', { defaultUnit: 'deg', postprocess: val => `rotate${Number(val) * 360}` })]
 
-  const docs: DocumentationPage = {
-    description: 'Windblade uses proportions instead of separete values.',
-    utilities: ['rotate-<theme.windblade.proportions>'],
-    preview: util => 'TODO',
-  }
+  const docs: DocumentationPage = `
+    <page>
+      <h1><title /></h1>
+      <p>Windblade uses proportions instead of separete values.</p>
+
+      <h2>Try it</h2>
+      <try-it selected="$util">
+        <utils>
+          <util>
+            rotate-
+            <select>
+              <for object="theme.windblade.proportions" key-as="$name" value-as="$value">
+                <option value="$name" />
+              </for>
+            </select>
+          </util>
+        </utils>
+
+        <renderer html="${encodeString(`
+          TODO
+        `)}" />
+
+        <h3>Preview</h3>
+        <viewport />
+
+        <h3>HTML</h3>
+        <html />
+
+        <h3>Generated CSS</h3>
+        <css />
+      </try-it>
+    </page>
+  `
 
   return { rules, docs }
 }
@@ -32,11 +89,39 @@ export const rotate = () => {
 export const translate = () => {
   const rules: Rule<theme.Theme>[] = [size.rule('translate', 'transform', { postprocess: val => `translate${val}` })]
 
-  const docs: DocumentationPage = {
-    description: 'X and Y variants have been removed because they are not logical properties. Windblade also uses proportions instead of separete values.',
-    utilities: ['translate-<theme.windblade.proportions>'],
-    preview: util => 'TODO',
-  }
+  const docs: DocumentationPage = `
+    <page>
+      <h1><title /></h1>
+      <p>X and Y variants have been removed because they are not logical properties. Windblade also uses proportions instead of separete values.</p>
+
+      <h2>Try it</h2>
+      <try-it selected="$util">
+        <utils>
+          <util>
+            translate-
+            <select>
+              <for object="theme.windblade.proportions" key-as="$name" value-as="$value">
+                <option value="$name" />
+              </for>
+            </select>
+          </util>
+        </utils>
+
+        <renderer html="${encodeString(`
+          TODO
+        `)}" />
+
+        <h3>Preview</h3>
+        <viewport />
+
+        <h3>HTML</h3>
+        <html />
+
+        <h3>Generated CSS</h3>
+        <css />
+      </try-it>
+    </page>
+  `
 
   return { rules, docs }
 }
@@ -44,11 +129,39 @@ export const translate = () => {
 export const skew = () => {
   const rules: Rule<theme.Theme>[] = [size.rule('skew', 'transform', { defaultUnit: 'deg', postprocess: val => `skew${Number(val) * 360}` })]
 
-  const docs: DocumentationPage = {
-    description: 'Windblade uses proportions instead of separete values.',
-    utilities: ['skew-<theme.windblade.proportions>'],
-    preview: util => 'TODO',
-  }
+  const docs: DocumentationPage = `
+    <page>
+      <h1><title /></h1>
+      <p>Windblade uses proportions instead of separete values.</p>
+
+      <h2>Try it</h2>
+      <try-it selected="$util">
+        <utils>
+          <util>
+            skew-
+            <select>
+              <for object="theme.windblade.proportions" key-as="$name" value-as="$value">
+                <option value="$name" />
+              </for>
+            </select>
+          </util>
+        </utils>
+
+        <renderer html="${encodeString(`
+          TODO
+        `)}" />
+
+        <h3>Preview</h3>
+        <viewport />
+
+        <h3>HTML</h3>
+        <html />
+
+        <h3>Generated CSS</h3>
+        <css />
+      </try-it>
+    </page>
+  `
 
   return { rules, docs }
 }
