421119d46b1353276a0d06a1b4ce1ed429020069
Herman Lederer <germans.lederers@gmail.com>
2023-03-10 20:35:09 +0100

    Update table docs to xml

--- packages/unocss-preset/src/rules/documented/tables.ts
+++ packages/unocss-preset/src/rules/documented/tables.ts
@@ -1,5 +1,6 @@
 import type { Rule } from '@unocss/core'
 import type { DocumentationPage } from 'unocss-docs'
+import { encodeString } from 'unocss-docs'
 import { ruleUtils } from '@windblade/core'
 import type { theme } from '@windblade/core'
 
@@ -11,11 +12,39 @@ export const borderSpacing = () => {
     // we are skiping border-spacing-b and borer-spacing-i for now beccause they are hard to implement
   ]
 
-  const docs: DocumentationPage = {
-    description: 'Changing border-spacing for individual axis is not possible at the moment.',
-    utilities: ['border-spacing-<theme.windblade.proportions>'],
-    preview: util => 'TODO',
-  }
+  const docs: DocumentationPage = `
+    <page>
+      <h1><title /></h1>
+      <p>Changing border-spacing for individual axis is not possible at the moment.</p>
+
+      <h2>Try it</h2>
+      <try-it selected="$util">
+        <utils>
+          <util>
+            border-spacing-
+            <select>
+              <for object="theme.windblade.proportions" key-as="$name" value-as="$value">
+                <option value="$name" />
+              </for>
+            </select>
+          </util>
+        </utils>
+
+        <renderer html="${encodeString(`
+          TODO
+        `)}" />
+
+        <h3>Preview</h3>
+        <viewport />
+
+        <h3>HTML</h3>
+        <html />
+
+        <h3>Generated CSS</h3>
+        <css />
+      </try-it>
+    </page>
+  `
 
   return { rules, docs }
 }
