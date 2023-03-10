9c60756104e5ddfbb6b3051919b90308a2613418
Herman Lederer <germans.lederers@gmail.com>
2023-03-10 18:52:02 +0100

    Update typography rules to xml

--- packages/unocss-preset/src/rules/documented/typography.ts
+++ packages/unocss-preset/src/rules/documented/typography.ts
@@ -1,15 +1,19 @@
 import type { Rule } from '@unocss/core'
 import type { DocumentationPage } from 'unocss-docs'
+import { encodeString } from 'unocss-docs'
 import { ruleUtils } from '@windblade/core'
 import type { theme } from '@windblade/core'
+import { iterObjects } from './doc-components'
 
 const { size } = ruleUtils
 
 export const fontFamily = () => {
-  const docs: DocumentationPage = {
-    description: 'Font family utilities have been removed.',
-    utilities: [],
-  }
+  const docs: DocumentationPage = `
+    <page>
+      <h1><title /></h1>
+      <p>Font family utilities have been removed.</p>
+    </page>
+  `
 
   return { rules: [], docs }
 }
@@ -17,20 +21,50 @@ export const fontFamily = () => {
 export const fontSize = () => {
   const rules: Rule<theme.Theme>[] = [size.rule('text', 'font-size')]
 
-  const docs: DocumentationPage = {
-    description: 'Windblade proportions are used instead of separate size values.',
-    utilities: ['text-<theme.windblade.proportions>'],
-    preview: util => `<div class="${util}">Lorem ipsum<div>`,
-  }
+  const docs: DocumentationPage = `
+    <page>
+      <h1><title /></h1>
+      <p>Windblade proportions are used instead of separate size values.</p>
+
+      <h2>Try it</h2>
+      <try-it selected="$util">
+        <utils>
+          <util>
+            text-
+            <select>
+              <for object="theme.windblade.proportions" key-as="$name" value-as="$value">
+                <option value="$name" />
+              </for>
+            </select>
+          </util>
+        </utils>
+
+        <renderer html="${encodeString(`
+          <div class="$util">Lorem ipsum<div>
+        `)}" />
+
+        <h3>Preview</h3>
+        <viewport />
+
+        <h3>HTML</h3>
+        <html />
+
+        <h3>Generated CSS</h3>
+        <css />
+      </try-it>
+    </page>
+  `
 
   return { rules, docs }
 }
 
 export const fontSmoothing = () => {
-  const docs: DocumentationPage = {
-    description: 'Font smoothing has been removed because Windblade sets it by default in preflight and it should never be changed. Plese open an issue if this is wrong.',
-    utilities: [],
-  }
+  const docs: DocumentationPage = `
+    <page>
+      <h1><title /></h1>
+      <p>Font smoothing has been removed because Windblade sets it by default in preflight and it should never be changed. Plese open an issue if this is wrong.</p>
+    </page>
+  `
 
   return { rules: [], docs }
 }
@@ -38,11 +72,39 @@ export const fontSmoothing = () => {
 export const tracking = () => {
   const rules: Rule<theme.Theme>[] = [size.rule('tracking', 'letter-spacing', { defaultUnit: 'em' })]
 
-  const docs: DocumentationPage = {
-    description: 'Windblade proportions are used instead of separate size values.',
-    utilities: ['tracking-<theme.windblade.proportions>'],
-    preview: util => `<div class="${util}">Lorem ipsum<div>`,
-  }
+  const docs: DocumentationPage = `
+    <page>
+      <h1><title /></h1>
+      <p>Windblade proportions are used instead of separate size values.</p>
+
+      <h2>Try it</h2>
+      <try-it selected="$util">
+        <utils>
+          <util>
+          tracking-
+            <select>
+              <for object="theme.windblade.proportions" key-as="$name" value-as="$value">
+                <option value="$name" />
+              </for>
+            </select>
+          </util>
+        </utils>
+
+        <renderer html="${encodeString(`
+          <div class="$util">Lorem ipsum<div>
+        `)}" />
+
+        <h3>Preview</h3>
+        <viewport />
+
+        <h3>HTML</h3>
+        <html />
+
+        <h3>Generated CSS</h3>
+        <css />
+      </try-it>
+    </page>
+  `
 
   return { rules, docs }
 }
@@ -50,11 +112,39 @@ export const tracking = () => {
 export const leading = () => {
   const rules: Rule<theme.Theme>[] = [size.rule('leading', 'line-height', { defaultUnit: '' })]
 
-  const docs: DocumentationPage = {
-    description: 'Windblade proportions are used instead of separate size values. Setting line height in rem units is not possible at the moment.',
-    utilities: ['leading-<theme.windblade.proportions>'],
-    preview: util => `<div class="${util} text-center" style="max-inline-size: 36ch;">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<div>`,
-  }
+  const docs: DocumentationPage = `
+    <page>
+      <h1><title /></h1>
+      <p>Windblade proportions are used instead of separate size values.</p>
+
+      <h2>Try it</h2>
+      <try-it selected="$util">
+        <utils>
+          <util>
+          leading-
+            <select>
+              <for object="theme.windblade.proportions" key-as="$name" value-as="$value">
+                <option value="$name" />
+              </for>
+            </select>
+          </util>
+        </utils>
+
+        <renderer html="${encodeString(`
+          <div class="$util text-center" style="max-inline-size: 36ch;">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<div>
+        `)}" />
+
+        <h3>Preview</h3>
+        <viewport />
+
+        <h3>HTML</h3>
+        <html />
+
+        <h3>Generated CSS</h3>
+        <css />
+      </try-it>
+    </page>
+  `
 
   return { rules, docs }
 }
@@ -65,15 +155,38 @@ export const textDecorationThickness = () => {
     size.rule('decoration', 'text-decoration-thickness'),
   ]
 
-  const docs: DocumentationPage = {
-    description: 'Windblade proportions are used instead of separate thickness values.',
-    utilities: [
-      'decoration-from-font',
-      'decoration-<theme.windblade.proportions>',
-      'decoration-<theme.windblade.miscSizes>',
-    ],
-    preview: util => `<div class="underline ${util}">Lorem ipsum<div>`,
-  }
+  const docs: DocumentationPage = `
+    <page>
+      <h1><title /></h1>
+      <p>Windblade proportions are used instead of separate thickness values.</p>
+
+      <h2>Try it</h2>
+      <try-it selected="$util">
+        <utils>
+          <util>decoration-from-font</util>
+          <util>
+            decoration-
+            <select>
+              ${iterObjects(['theme.windblade.proportions', 'theme.windblade.miscSizes'], '$name', '$value', '<option value="$name" />')}
+            </select>
+          </util>
+        </utils>
+
+        <renderer html="${encodeString(`
+          <div class="underline $util">Lorem ipsum<div>
+        `)}" />
+
+        <h3>Preview</h3>
+        <viewport />
+
+        <h3>HTML</h3>
+        <html />
+
+        <h3>Generated CSS</h3>
+        <css />
+      </try-it>
+    </page>
+  `
 
   return { rules, docs }
 }
@@ -81,14 +194,37 @@ export const textDecorationThickness = () => {
 export const textUnderlineOffset = () => {
   const rules: Rule<theme.Theme>[] = [size.rule('underline-offset', 'text-underline-offset')]
 
-  const docs: DocumentationPage = {
-    description: 'Windblade proportions are used instead of separate ofset values.',
-    utilities: [
-      'underline-offset-<theme.windblade.proportions>',
-      'underline-offset-<theme.windblade.miscSizes>',
-    ],
-    preview: util => `<div class="underline ${util}">Lorem ipsum<div>`,
-  }
+  const docs: DocumentationPage = `
+    <page>
+      <h1><title /></h1>
+      <p>Windblade proportions are used instead of separate ofset values.</p>
+
+      <h2>Try it</h2>
+      <try-it selected="$util">
+        <utils>
+          <util>
+            underline-offset-
+            <select>
+              ${iterObjects(['theme.windblade.proportions', 'theme.windblade.miscSizes'], '$name', '$value', '<option value="$name" />')}
+            </select>
+          </util>
+        </utils>
+
+        <renderer html="${encodeString(`
+          <div class="underline $util">Lorem ipsum<div>
+        `)}" />
+
+        <h3>Preview</h3>
+        <viewport />
+
+        <h3>HTML</h3>
+        <html />
+
+        <h3>Generated CSS</h3>
+        <css />
+      </try-it>
+    </page>
+  `
 
   return { rules, docs }
 }
