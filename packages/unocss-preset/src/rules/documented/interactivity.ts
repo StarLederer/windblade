e6b6fb84a046962d6bcaa01fe601892109a0ebf9
Herman Lederer <germans.lederers@gmail.com>
2023-03-10 20:34:59 +0100

    Update interactivity docs to xml

--- packages/unocss-preset/src/rules/documented/interactivity.ts
+++ packages/unocss-preset/src/rules/documented/interactivity.ts
@@ -1,21 +1,51 @@
 import type { Rule } from '@unocss/core'
 import type { DocumentationPage } from 'unocss-docs'
+import { encodeString } from 'unocss-docs'
 import { ruleUtils } from '@windblade/core'
 import type { theme } from '@windblade/core'
+import { selectLogical } from './doc-components'
 
 const { logical, size } = ruleUtils
 
 export const scrollMargin = () => {
   const rules: Rule<theme.Theme>[] = size.edgeRules('scroll-m', '', 'scroll-margin', '')
 
-  const docs: DocumentationPage = {
-    description: 'Windblade proportions are used instead of separate size values, and physical properties are replaced with logical.',
-    utilities: [
-      ...Object.keys(logical.abbreviations.axis).map(val => `scroll-m-${val}`),
-      ...Object.keys(logical.abbreviations.edges).map(val => `scroll-m-${val}`),
-    ].flatMap(val => [`${val}-<theme.windblade.proportions>`, `${val}-<theme.windblade.miscSizes>`]),
-    preview: util => 'TODO',
-  }
+  const docs: DocumentationPage = `
+    <page>
+      <h1><title /></h1>
+      <p>Windblade proportions are used instead of separate size values.</p>
+
+      <h2>Try it</h2>
+      <try-it selected="$util">
+        <utils>
+          ${[false, true].map(val => `
+            <util>
+              scroll-m-
+              ${val ? `${selectLogical({ axis: true, edges: true })}-` : ''}
+              <select>
+                <for object="theme.windblade.proportions" key-as="$name" value-as="$value">
+                  <option value="$name" />
+                </for>
+              </select>
+            </util>
+          `).join('')}
+        </utils>
+
+        <renderer html="${encodeString(`
+          TODO
+        `)}" />
+
+        <h3>Preview</h3>
+        <viewport />
+
+        <h3>HTML</h3>
+        <html />
+
+        <h3>Generated CSS</h3>
+        <css />
+      </try-it>
+    </page>
+  `
 
   return { rules, docs }
 }
@@ -23,14 +53,42 @@ export const scrollMargin = () => {
 export const scrollPadding = () => {
   const rules: Rule<theme.Theme>[] = size.edgeRules('scroll-p', '', 'scroll-padding', '')
 
-  const docs: DocumentationPage = {
-    description: 'Windblade proportions are used instead of separate size values, and physical properties are replaced with logical.',
-    utilities: [
-      ...Object.keys(logical.abbreviations.axis).map(val => `scroll-p-${val}`),
-      ...Object.keys(logical.abbreviations.edges).map(val => `scroll-p-${val}`),
-    ].flatMap(val => [`${val}-<theme.windblade.proportions>`, `${val}-<theme.windblade.miscSizes>`]),
-    preview: util => 'TODO',
-  }
+  const docs: DocumentationPage = `
+    <page>
+      <h1><title /></h1>
+      <p>Windblade proportions are used instead of separate size values, and physical properties are replaced with logical.</p>
+
+      <h2>Try it</h2>
+      <try-it selected="$util">
+        <utils>
+          ${[false, true].map(val => `
+            <util>
+              scroll-p-
+              ${val ? `${selectLogical({ axis: true, edges: true })}-` : ''}
+              <select>
+                <for object="theme.windblade.proportions" key-as="$name" value-as="$value">
+                  <option value="$name" />
+                </for>
+              </select>
+            </util>
+          `).join('')}
+        </utils>
+
+        <renderer html="${encodeString(`
+          TODO
+        `)}" />
+
+        <h3>Preview</h3>
+        <viewport />
+
+        <h3>HTML</h3>
+        <html />
+
+        <h3>Generated CSS</h3>
+        <css />
+      </try-it>
+    </page>
+  `
 
   return { rules, docs }
 }
@@ -43,11 +101,35 @@ export const scrollSnapType = () => {
     ['snap-proximity', { '--wb-scroll-snap-strictness': 'proximity' }],
   ]
 
-  const docs: DocumentationPage = {
-    description: 'snap-x and snap-y have been removed because they have no logical counterparts yet.',
-    utilities: ['snap-none', 'snap-both', 'snap-mandatory', 'snap-proximity'],
-    preview: util => 'TODO',
-  }
+  const docs: DocumentationPage = `
+    <page>
+      <h1><title /></h1>
+      <p><code>snap-x</code> and <code>snap-y</code> have been removed because they have no logical counterparts yet.</p>
+
+      <h2>Try it</h2>
+      <try-it selected="$util">
+        <utils>
+          <util>snap-none</util>
+          <util>snap-both</util>
+          <util>snap-mandatory</util>
+          <util>snap-proximity</util>
+        </utils>
+
+        <renderer html="${encodeString(`
+          TODO
+        `)}" />
+
+        <h3>Preview</h3>
+        <viewport />
+
+        <h3>HTML</h3>
+        <html />
+
+        <h3>Generated CSS</h3>
+        <css />
+      </try-it>
+    </page>
+  `
 
   return { rules, docs }
 }
@@ -60,11 +142,35 @@ export const touchAction = () => {
     ['touch-manipulation', { 'touch-action': 'manipulation' }],
   ]
 
-  const docs: DocumentationPage = {
-    description: 'pan touch-actions have been removed because they have no logical counterparts yet.',
-    utilities: ['touch-auto', 'touch-none', 'touch-pinch-zoom', 'touch-manipulation'],
-    preview: util => 'TODO',
-  }
+  const docs: DocumentationPage = `
+    <page>
+      <h1><title /></h1>
+      <p><code>pan</code> touch-actions have been removed because they have no logical counterparts yet.</p>
+
+      <h2>Try it</h2>
+      <try-it selected="$util">
+        <utils>
+          <util>touch-auto</util>
+          <util>touch-none</util>
+          <util>touch-pinch-zoom</util>
+          <util>touch-manipulation</util>
+        </utils>
+
+        <renderer html="${encodeString(`
+          TODO
+        `)}" />
+
+        <h3>Preview</h3>
+        <viewport />
+
+        <h3>HTML</h3>
+        <html />
+
+        <h3>Generated CSS</h3>
+        <css />
+      </try-it>
+    </page>
+  `
 
   return { rules, docs }
 }
