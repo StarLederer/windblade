0d3e61feb7812e2f19d347c44f13e3dc322ae70c
Herman Lederer <germans.lederers@gmail.com>
2023-03-12 17:53:02 +0100

    Update variant docs to xml

--- packages/unocss-preset/src/docs/usage/variants.ts
+++ packages/unocss-preset/src/docs/usage/variants.ts
@@ -1,9 +1,10 @@
 import type { DocumentationPage } from 'unocss-docs'
 
-const colors: DocumentationPage
-= `# Hover, focus and other states
-
-Windblade does not come with functionality like hover or focus states. Please use Windblade together with [unocss-preset-mini-variants](https://www.npmjs.com/package/unocss-preset-mini-variants) or other variant implementations if you need this functionality.
+const main: DocumentationPage = `
+  <page>
+    <h1>Hover, focus and other states</h1>
+    <p>Windblade does not come with functionality like hover or focus states. Please use Windblade together with <a href="https://www.npmjs.com/package/unocss-preset-mini-variants">unocss-preset-mini-variants</a> or other variant implementations if you need this functionality.</p>
+  </page>
 `
 
-export default colors
+export default main
