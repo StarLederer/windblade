42956c82261a00c1bd683ccbc40756d3afead4dd
Star Lederer <germans.lederers@gmail.com>
2023-04-10 13:48:09 +0200

    Make `Outlet` wrapper component more customizable

--- packages/website/src/lib/rotuer/Outlet.tsx
+++ packages/website/src/lib/rotuer/Outlet.tsx
@@ -1,15 +1,25 @@
-import type { Component } from 'solid-js'
+import type { Component, ParentComponent } from 'solid-js'
 import { Outlet } from '@solidjs/router'
 import { Transition } from 'solid-transition-group'
 
 const style = 'animation-duration-m.4'
 
-const Main: Component = () => (
-  <section class="relative size-i-full size-b-full overflow-hidden">
-    <Transition enterActiveClass={`${style} animate-in`} exitActiveClass={`${style} animate-out`}>
-      <Outlet />
-    </Transition>
-  </section>
-)
+const Main: Component<{
+  class?: string
+  as?: ParentComponent<{
+    class: string
+  }>
+}> = (props) => {
+  const className = () => `${props.class} relative size-i-full size-b-full overflow-hidden`
+  const Container = props.as ?? (props => <section class={className()}>{props.children}</section>)
+
+  return (
+    <Container class={className()}>
+      <Transition enterActiveClass={`${style} animate-in`} exitActiveClass={`${style} animate-out`}>
+        <Outlet />
+      </Transition>
+    </Container>
+  )
+}
 
 export default Main
