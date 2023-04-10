2da40641b60976ee0ce5dba3428f916334c373dd
Star Lederer <germans.lederers@gmail.com>
2023-04-10 13:33:13 +0200

    Remove a useless `<div>` and replace a custom element with `Page` component

--- packages/website/src/tree/branches/Docs/components/Page.tsx
+++ packages/website/src/tree/branches/Docs/components/Page.tsx
@@ -4,36 +4,35 @@ import type { DocumentationPage } from 'unocss-docs'
 import Error from './Page/components/Error'
 import XmlRoot from './Page/XmlRoot'
 import libs from '~/lib/external'
+import { Page } from '~/lib/rotuer'
 
 const Main: Component<{
   title: string
   page: DocumentationPage
 }> = (props) => {
   return (
-    <div class="absolute size-i-full size-b-full overflow-auto">
-      <div class="p-m.2 flex flex-col gap-s">
-        <Suspense fallback="Loading...">
-          {(() => {
-            const parser = libs.xml()
-            if (!parser)
-              return 'Error: Failed to load XML parser'
+    <Page class="p-m.2 flex flex-col gap-s">
+      <Suspense fallback="Loading...">
+        {(() => {
+          const parser = libs.xml()
+          if (!parser)
+            return 'Error: Failed to load XML parser'
 
-            let xml
-            try {
-              xml = parser.fromXml(props.page)
-            }
-            catch (err: unknown) {
-              return <Error>Error parsing this page: {err as string}</Error>
-            }
+          let xml
+          try {
+            xml = parser.fromXml(props.page)
+          }
+          catch (err: unknown) {
+            return <Error>Error parsing this page: {err as string}</Error>
+          }
 
-            if (xml)
-              return <XmlRoot {...xml} title={props.title}/>
+          if (xml)
+            return <XmlRoot {...xml} title={props.title} />
 
-            return <Error>Error processing this page</Error>
-          })()}
-        </Suspense>
-      </div>
-    </div>
+          return <Error>Error processing this page</Error>
+        })()}
+      </Suspense>
+    </Page>
   )
 }
 
