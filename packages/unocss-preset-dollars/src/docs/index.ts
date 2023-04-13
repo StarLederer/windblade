f9db6bda64085f30db8039f83c49394abb8018ee
Star Lederer <germans.lederers@gmail.com>
2023-04-13 22:13:40 +0200

    Add dollar preset installation guide

--- packages/unocss-preset-dollars/src/docs/index.ts
+++ packages/unocss-preset-dollars/src/docs/index.ts
@@ -1,9 +1,11 @@
 import type { DocumentationTree } from 'unocss-docs'
 import { compiler } from 'unocss-docs'
+import installation from './installation'
 import dollarSyntax from './dollarSyntax'
 
 const main: DocumentationTree = new Map([
   ['Usage', new Map([
+    ['Installation', installation],
     ['$ syntax', dollarSyntax],
   ])],
 ])
---
+++ packages/unocss-preset-dollars/src/docs/installation.ts
@@ -0,0 +1,25 @@
+import type { DocumentationPage } from 'unocss-docs'
+import { encodeString } from 'unocss-docs'
+
+const example = `
+import { defineConfig } from 'unocss';
+import windbladeDollars from '@windblade/preset-dollars';
+
+export default defineConfig({
+  presets: [
+    windbladeDollars(),
+  ],
+});`
+
+const main: DocumentationPage = `
+  <page>
+    <h1><title /></h1>
+    <p>Just like the complete Windblade, the Dollars module is an UnoCSS preset, please follow its own <a href="https://github.com/unocss/unocss#installation">guide</a> to install it.</p>
+    <p>Once UnoCSS is installed in your project simply get @windblade/preset-dollars from npm and add it to the presets array.</p>
+    <pre lang="sh" code="npm install @windblade/preset-color" />
+    <pre lang="ts" code="${encodeString(example)}" />
+    <p>That's it! You can now use Windblade's dollar syntax in your project.</p>
+  </page>
+`
+
+export default main
