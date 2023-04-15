51381441109a1676cfa64d6005f610745b158c52
Star Lederer <germans.lederers@gmail.com>
2023-04-15 22:41:04 +0200

    Fix optional prop appearing required

--- packages/website/src/lib/Select.tsx
+++ packages/website/src/lib/Select.tsx
@@ -3,7 +3,7 @@ import type { ParentComponent } from 'solid-js'
 const Main: ParentComponent<{
   id?: string
   class?: string
-  onChange: (e: Event) => void
+  onChange?: (e: Event) => void
 }> = props => (
   <div class="relative">
     <select {...props} class={`${props.class} transition size-i-full appearance-none p-s p-ie-$($s*3) hover:highlight active:highlight+`} onChange={props.onChange}>
