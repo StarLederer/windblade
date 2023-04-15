fb97245d717f916f4d1fa89b8df0ddb609314507
Star Lederer <germans.lederers@gmail.com>
2023-04-15 22:24:26 +0200

    Implement addressable module storage

--- packages/website/src/stores/docsStore.ts
+++ packages/website/src/stores/docsStore.ts
@@ -4,9 +4,30 @@ import type { Module, ModuleId, ModuleMeta, Option } from '~/api'
 import { get, getIndex } from '~/api'
 
 function main() {
+  const modules = new Map<ModuleId, Module>()
+
   const [index, setIndex] = createSignal<Option<Map<ModuleId, ModuleMeta>, string>>()
   const [module, setModule] = createSignal<Option<Module, string>>()
   const [moduleId, setModuleId] = createSignal<ModuleId>()
+
+  const getModuleById = async (id: ModuleId): Promise<Option<Module, string>> => {
+    const cached = modules.get(id)
+
+    if (cached) {
+      return {
+        success: true,
+        value: cached,
+      }
+    }
+    else {
+      const mdle = await get(id)
+
+      if (mdle.success)
+        modules.set(id, mdle.value)
+
+      return mdle
+    }
+  }
 
   const fetchIndex = async () => {
     setIndex(getIndex())
@@ -21,7 +42,7 @@ function main() {
     setModule(await get(id))
   }
 
-  return { index, fetchIndex, module, moduleId, fetchModule }
+  return { index, fetchIndex, module, moduleId, fetchModule, getModuleById }
 }
 
 export default createRoot(main)
