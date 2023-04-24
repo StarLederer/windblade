7fa9aa39d03bdc121272afd557dc162581183f4a
Star Lederer <germans.lederers@gmail.com>
2023-04-24 23:04:37 +0200

    Remove old unused deps

--- packages/website/src/lib/external.ts
+++ packages/website/src/lib/external.ts
@@ -4,9 +4,6 @@ const main = () => {
   const [formatter] = createResource(async () => (await import('js-beautify')).default)
   const [highlighter] = createResource(async () => (await import('highlight.js')).default)
   const [xml] = createResource(async () => (await import('xast-util-from-xml')))
-  // const [md] = createResource(async () => (await import('mdast-util-from-markdown')))
-  // const [mdDirective] = createResource(async () => (await import('mdast-util-directive')))
-  // const [micromarkDirective] = createResource(async () => (await import('micromark-extension-directive')))
   return { highlighter, formatter, xml }
 }
 
