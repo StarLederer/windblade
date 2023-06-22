ca4fd4219e5f2cb8669670d8cb8f39662615f823
Star Lederer <germans.lederers@gmail.com>
2023-06-22 12:05:20 +0200

    feat: improve handling of chapter rotues

--- packages/website/src/router/pages/docs/[id]/[...all].tsx
+++ packages/website/src/router/pages/docs/[id]/[...all].tsx
@@ -1,15 +1,9 @@
 import { useNavigate, useParams } from '@solidjs/router'
+import Button from '@ui/primitives/Button'
 import type { CompiledDocumentationTree } from '@windblade/unocss-docs/src'
 import type { Component } from 'solid-js'
 import { Page } from '~/lib/rotuer'
 import docsStore from '~/stores/docsStore'
-
-const Hint: Component = () => (
-  <Page class="p-m.2 flex gap-s text-m.2 items-center font-bold">
-    <div class="i-mdi-arrow-left" />
-    Select something
-  </Page>
-)
 
 const findFirstPage = (docs: CompiledDocumentationTree, prefix: string[] = []): string[] | undefined => {
   for (let i = 0; i < docs.length; ++i) {
@@ -31,7 +25,7 @@ const Main: Component = () => {
   const params = useParams()
   const navigate = useNavigate()
 
-  ;(async () => {
+  const tryOpenFirstPage = async () => {
     const mdle = await docsStore.getModuleById(params.moduleId)
 
     if (!mdle.success)
@@ -41,11 +35,20 @@ const Main: Component = () => {
 
     if (pageAddress)
       navigate(`/docs/${params.moduleId}/${pageAddress.join('/')}`)
-  })()
+  }
 
   return (
-    <Page class="p-m.2">
-      Redirecting...
+    <Page class="p-m.2 flex flex-col gap-s justify-center items-center text-center font-semibold">
+      <h2 class="text-fg-3 text-s">Navigation error</h2>
+      <p class="text-m.2 font-bold">
+        The page you are looking for is now a whole chapter.
+      </p>
+      <p class="text-$($s+$s.2)">
+        Use the navigation menu to open any page inside this chapter.
+      </p>
+      <Button style="half" onClick={tryOpenFirstPage}>
+        Or find first page
+      </Button>
     </Page>
   )
 }
