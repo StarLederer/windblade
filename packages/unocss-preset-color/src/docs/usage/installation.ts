f5f5c29dfd33db4de9994d4c7076a70e9baac275
Star Lederer <germans.lederers@gmail.com>
2023-06-27 15:57:36 +0200

    docs: fix package names

--- packages/unocss-preset-color/src/docs/usage/installation.ts
+++ packages/unocss-preset-color/src/docs/usage/installation.ts
@@ -3,7 +3,7 @@ import { encodeString } from 'unocss-docs'
 
 const example = `
 import { defineConfig } from 'unocss';
-import windbladeColor from '@windblade/preset-color';
+import windbladeColor from '@windblade/unocss-preset-color';
 
 export default defineConfig({
   presets: [
@@ -12,7 +12,7 @@ export default defineConfig({
 });`
 
 const removeColors = `import { defineConfig, presetMini } from 'unocss';
-import windbladeColor from '@windblade/preset-color';
+import windbladeColor from '@windblade/unocss-preset-color';
 
 export default defineConfig({
   extendTheme: [
@@ -30,13 +30,13 @@ const main: DocumentationPage = `
   <page>
     <h1><title /></h1>
     <p>Just like the complete Windblade, the Color module is an UnoCSS preset, please follow its own <a href="https://unocss.dev/integrations">guide</a> to install it.</p>
-    <p>Once UnoCSS is installed in your project simply get @windblade/preset-color from npm and add it to the presets array.</p>
-    <pre lang="sh" code="npm install @windblade/preset-color" />
+    <p>Once UnoCSS is installed in your project simply get @windblade/unocss-preset-color from npm and add it to the presets array.</p>
+    <pre lang="sh" code="npm install @windblade/unocss-preset-color" />
     <pre lang="ts" code="${encodeString(example)}" />
     <p>That's it! You can now use Windblade's color utilities in your proejct but it is likely that you want more utilities for things like layout and typography, please read on to learn how to use Windblade's color module othether with other UnoCSS presets.</p>
 
     <h2>Installation with other UnoCSS presets</h2>
-    <p>You likely want to combine @windblade/preset-color with other UnoCSS presets (e.g. @unocss/preset-mini) to get utilities for layout, typography and other CSS features. This should generally work without any problems but we recommend removing colors from other presets' configuration to avoid confusion and possible conflicts.</p>
+    <p>You likely want to combine @windblade/unocss-preset-color with other UnoCSS presets (e.g. @unocss/preset-mini) to get utilities for layout, typography and other CSS features. This should generally work without any problems but we recommend removing colors from other presets' configuration to avoid confusion and possible conflicts.</p>
     <p>You will find instructions on how to remove colors from most popuilar UnoCSS presets below. If you are using other presets consult the docs for those presets.</p>
 
     <h3>@unocss/preset-mini, @unocss/preset-wind &amp; @unocss/preset-uno</h3>
--- packages/unocss-preset-dollars/src/docs/installation.ts
+++ packages/unocss-preset-dollars/src/docs/installation.ts
@@ -3,7 +3,7 @@ import { encodeString } from 'unocss-docs'
 
 const example = `
 import { defineConfig } from 'unocss';
-import windbladeDollars from '@windblade/preset-dollars';
+import windbladeDollars from '@windblade/unocss-preset-dollars';
 
 export default defineConfig({
   presets: [
@@ -15,8 +15,8 @@ const main: DocumentationPage = `
   <page>
     <h1><title /></h1>
     <p>Just like the complete Windblade, the Dollars module is an UnoCSS preset, please follow its own <a href="https://unocss.dev/integrations">guide</a> to install it.</p>
-    <p>Once UnoCSS is installed in your project simply get @windblade/preset-dollars from npm and add it to the presets array.</p>
-    <pre lang="sh" code="npm install @windblade/preset-color" />
+    <p>Once UnoCSS is installed in your project simply get @windblade/unocss-preset-dollars from npm and add it to the presets array.</p>
+    <pre lang="sh" code="npm install @windblade/unocss-preset-dollars" />
     <pre lang="ts" code="${encodeString(example)}" />
     <p>That's it! You can now use Windblade's dollar syntax in your project.</p>
   </page>
